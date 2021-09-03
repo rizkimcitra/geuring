db42dde7c1cbb55854ba1c30134a48bccbd51390
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2021-09-03 23:54:50 +0700

    menghapus toggle dark mode dan mneyesuaikan kode

--- src/util/Theme.js
+++ src/util/Theme.js
@@ -1,29 +1,11 @@
 export default function Theme() {
-  const themeSwitcher = document.getElementById("darkModeToggle")
-  const themeSwitcherIcon = document.getElementById("toggleIcon")
-  const changeTheme = () => {
-    const color = localStorage.getItem("theme")
-    if (color) {
-      document.documentElement.classList.add(color)
-      themeSwitcherIcon.classList.remove("bx-moon")
-      themeSwitcherIcon.classList.add("bx-sun")
-    } else {
-      document.documentElement.classList.remove("dark")
-      themeSwitcherIcon.classList.add("bx-moon")
-      themeSwitcherIcon.classList.remove("bx-sun")
-    }
+  document.getElementById("close-modal").addEventListener("click", closeModal)
+  function closeModal() {
+    const modal = document.getElementById("modal-error")
+    const modalBody = document.getElementById("modal-body")
+    modal.classList.toggle("scale-0")
+    document.body.classList.toggle("overflow-y-hidden")
+    modal.classList.toggle("scale-100")
+    modalBody.innerHTML = ""
   }
-
-  changeTheme()
-
-  themeSwitcher.addEventListener("click", function () {
-    const color = localStorage.getItem("theme")
-    if (color) {
-      localStorage.removeItem("theme")
-      changeTheme()
-    } else {
-      localStorage.setItem("theme", "dark")
-      changeTheme()
-    }
-  })
 }
